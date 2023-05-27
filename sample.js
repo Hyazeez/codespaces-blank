d3b771e1239ed4d2e0975177c911ecacc22a8f7d
Azeez shaban <118077405+Hyazeez@users.noreply.github.com>
2023-05-27 04:37:00 +0000

    first commit

---
+++ sample.js
@@ -0,0 +1,29 @@
+function remainder(fn,sn){
+    return fn % sn;
+}
+console.log(remainder(10,3));
+1
+
+function add(fn,sn){
+    return fn + sn;
+}
+console.log(add(10,3));
+13
+
+function subtract(fn,sn){
+    return fn - sn;
+}
+console.log(subtract(10,3));
+7
+
+function multiplication(fn,sn){
+    return fn * sn;
+}
+console.log(multiplication(10,3));
+30
+
+function division(fn,sn){
+    return fn / sn;
+}
+console.log(division(10,3));
+3.3333333333333335
